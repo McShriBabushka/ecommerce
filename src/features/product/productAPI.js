17195058f9846fc9996f72297f71bc1845cf0cf4
McShriBabushka <reeshavthealpha@gmail.com>
2024-07-25 23:02:59 +0530

    fetching from dummyjson api and sorting/filtering applied

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,8 +1,31 @@
-export function fetchCount(amount = 1) {
+//todo: we're not gonna hardcode the server .. it's supposed to be able
+//to fetch data from anywhere
+export function fetchAllProducts() {
     return new Promise(async (resolve) =>{
-      const response = await fetch('http://') 
+      try{const response = await fetch('http://localhost:8080/products') 
+      if(!response )console.log('network response not ok');
       const data = await response.json()
-      resolve({data})
+      resolve({data})}
+      catch(error){
+           console.log(error);
+      }
     }
     );
   }
+  export function fetchProductsByFilter(filter) {
+    let querystring='';
+    for(let key in filter){
+     querystring+= `${key}=${filter[key]}&`
+    }
+    querystring=querystring.slice(0,-1);
+    console.log(querystring);
+    
+    return new Promise(async (resolve) =>{
+      console.log(querystring);
+      const response = await fetch('http://localhost:8080/products?&'+querystring) 
+      if(!response )console.log('network response not ok');
+      const data = await response.json()
+      resolve({data})}
+    );
+  }
+
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import Cart from '../features/cart/Cart'
+import React from "react";
+import Cart from "../features/cart/Cart";
+import Navbar from "../features/navbar/Navbar";
 export default function CartPage() {
   return (
-    <div><Cart></Cart></div>
-  )
+    <div>
+      <Navbar>
+        <Cart></Cart>
+      </Navbar>
+    </div>
+  );
 }
