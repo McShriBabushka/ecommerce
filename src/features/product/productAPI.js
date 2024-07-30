7c1368aa8293387325ccb5c9f455e303d184f05b
McShriBabushka <reeshavthealpha@gmail.com>
2024-07-30 14:44:31 +0530

    path from product grid to product details set up

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -13,12 +13,14 @@ export function fetchAllProducts() {
   });
 }
 
-export function fetchProductsByFilter({filter,sort}) {
+export function fetchProductsByFilter({filter,sort,pagination}) {
   //TODO:
   //filter= {"category":"smartphone","laptops"}..something like that
   //sort= {_sort:"price",_order="asc"}
+  //pagination ={_page:1,_limit=10}
   //on server we'll support multi values
   let querystring = '';
+  //category filter
   for (let key in filter) {
     const categoryValues = filter[key];
     if (categoryValues.length) {
@@ -26,19 +28,57 @@ export function fetchProductsByFilter({filter,sort}) {
       querystring += `${key}=${lastCategoryValue}&`;
     }
   }
+  //sort
   for(let key in sort){
     querystring += `${key}=${sort[key]}&`
   }
+  //pagination
+  for(let key in pagination){
+     querystring += `${key}=${pagination[key]}&`
+  }
   querystring= querystring.slice(0,-1);
-  console.log(querystring);
-
+  
   return new Promise(async (resolve) => {
     console.log(querystring);
     const response = await fetch(
-      "http://localhost:8080/products?&" + querystring
+      "http://localhost:8080/products?" + querystring
     );
     if (!response) console.log("network response not ok");
     const data = await response.json();
-    resolve({ data });
+    const totalItems= await response.headers.get('X-Total-Count');
+    resolve({ data:{products:data,totalItems:totalItems} });
+
+  });
+}
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    try {
+      const response = await fetch("http://localhost:8080/categories");
+      if (!response) console.log("network response not ok");
+      const data = await response.json();
+      resolve({ data });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    try {
+      const response = await fetch("http://localhost:8080/brands");
+      if (!response) console.log("network response not ok");
+      const data = await response.json();
+      resolve({ data });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+export function fetchProductById(id) {
+  return new Promise(async (resolve) => {
+      const response = await fetch('http://localhost:8080/products/'+id);
+      const data = await response.json();
+      resolve({ data });
   });
 }
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,9 +1,13 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts, fetchProductsByFilter, } from './ProductAPI';
+import { fetchAllProducts, fetchProductsByFilter,fetchBrands,fetchCategories, fetchProductById } from './ProductAPI';
 
 const initialState = {
   products: [],
+  brands:[],
+  categories:[],
   status: 'idle' ,
+  totalItems:0,
+  selectedProduct: null
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
@@ -14,14 +18,40 @@ export const fetchAllProductsAsync = createAsyncThunk(
     return response.data;
   }
 );
-export const fetchProductsByFilterAsync = createAsyncThunk(
-  'product/fetchProductsByFilter',
-  async ({filter,sort}) => {
-    const response = await fetchProductsByFilter({filter,sort});
+export const fetchBrandsAsync = createAsyncThunk(
+  'product/fetchBrands',
+  async () => {
+    const response = await fetchBrands();
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
+export const fetchCategoriesAsync = createAsyncThunk(
+  'product/fetchCategories',
+  async () => {
+    const response = await fetchCategories();
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const fetchProductsByFilterAsync = createAsyncThunk(
+  'product/fetchProductsByFilter',
+  async ({filter,sort,pagination}) => {
+    const response = await fetchProductsByFilter({filter,sort,pagination});
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const fetchAllProductsByIdAsync= createAsyncThunk(
+  'product/fetchProductById',
+  async(id)=>{
+    const response = await fetchProductById(id);
+    return response.data;
+  }
+
+)
+
 
 export const productSlice = createSlice({
   name: 'product',
@@ -45,7 +75,29 @@ export const productSlice = createSlice({
       })
       .addCase(fetchProductsByFilterAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.products = action.payload;
+        state.products = action.payload.products;
+        state.totalItems = action.payload.totalItems;
+      })
+      .addCase(fetchBrandsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchBrandsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.brands = action.payload;
+      })
+      .addCase(fetchCategoriesAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCategoriesAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.categories = action.payload;
+      })
+      .addCase(fetchAllProductsByIdAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllProductsByIdAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.selectedProduct = action.payload;
       })
   },
 });
@@ -53,5 +105,9 @@ export const productSlice = createSlice({
 export const { increment } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
+export const selectBrands = (state) => state.product.brands;
+export const selectCategories = (state) => state.product.categories;
+export const selectProductById= (state) => state.product.selectedProduct;
+export const selectAllItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer;
